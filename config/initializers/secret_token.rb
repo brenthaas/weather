73d257d7425769704ec05dd62ebaf339ec4b8cec
Brent <brenthaas+git@gmail.com>
2012-11-10 01:25:20 -0800

    initial commit - nothing here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weather::Application.config.secret_token = 'ca78097f591cd2bf1bff4cc9a925128cc541e941e641329ccbd00a666e6e52fa67248d7573016b679efc100303a77067082c6fbbecfd2059abcd3437d76b1f6e'
