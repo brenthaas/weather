9cee4d5f3316ffedc8429c01709f018a79cd5dc3
Brent <brenthaas+git@gmail.com>
2012-11-12 02:03:38 -0800

    Added and tested validators

--- app/models/conditions.rb
+++ app/models/conditions.rb
@@ -1,4 +1,13 @@
 class Conditions < ActiveRecord::Base
   belongs_to :location
   attr_accessible :temp, :wind_direction, :wind_speed
+
+  validates :wind_speed, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :wind_direction, :format => { 
+  	:with => /^[NS]?[NSWE]?$/i,
+  	:message => "Wind direction should consist of [NSWE]" }
+
+  def wind_direction=(dir)
+  	write_attribute :wind_direction, dir.upcase
+  end
 end
--- spec/models/conditions_spec.rb
+++ spec/models/conditions_spec.rb
@@ -1,8 +1,38 @@
 require 'spec_helper'
 
 describe Conditions do
+	before do 
+		@weather = Conditions.create(:temp => 75.5, :wind_speed => 12, :wind_direction => "NW")
+	end
+	subject { @weather }
 	it { should respond_to :location }
 	it { should respond_to :temp }
 	it { should respond_to :wind_speed }
 	it { should respond_to :wind_direction }
+	it { should be_valid }
+
+	describe "validators" do
+		it "does not accept negative wind speed" do
+			@weather.wind_speed = -5
+			should_not be_valid
+		end
+
+		context "don't accept weird wind directions" do
+			specify "with invalid characters" do
+				@weather.wind_direction = "dn"
+				should_not be_valid
+			end
+			specify "long directions" do
+				@weather.wind_direction = "swe"
+				should_not be_valid
+			end
+		end
+	end
+
+	describe "normalizing" do
+		it "upcases wind direction" do
+			@weather.wind_direction = "sw"
+			@weather.wind_direction.should == "SW"
+		end
+	end
 end
