b2f9843047d8e1ed8d131c58f028860264f0f792
Brent <brenthaas+git@gmail.com>
2012-11-12 22:25:51 -0800

    Added conditions update

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,6 +14,7 @@ class LocationsController < ApplicationController
   # GET /locations/1.json
   def show
     @location = Location.find(params[:id])
+    @conditions = @location.current_conditions
 
     respond_to do |format|
       format.html # show.html.erb
