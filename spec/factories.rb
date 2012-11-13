cf838500ec4e29ea15ee70a0b53e49162aeb6f66
Brent <brenthaas+git@gmail.com>
2012-11-13 01:10:49 -0800

    Added a list of conditions to location

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,25 @@
 FactoryGirl.define do
-	factory :location do
-		sequence(:name) { |num| "Test Location #{num}"}
-	end
 
 	factory :conditions do
+		location
 		temp 80
 		wind_speed 15
 		wind_direction 'SW'
+	end
+
+	factory :location do
+		sequence(:name) { |num| "Test Location #{num}"}
+
+		factory :location_with_conditions do
+			ignore do
+				conditions_count 15
+			end
+
+			after(:create) do |location, evaluator|
+				FactoryGirl.create_list(:conditions, 
+					evaluator.conditions_count, 
+					:location => location)
+			end
+		end
 	end
 end
