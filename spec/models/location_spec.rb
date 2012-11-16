e598552c5802cb902f5470ca7de19768faaadbe7
Brent <brenthaas+git@gmail.com>
2012-11-16 15:28:29 -0800

    added specs for validations - switched to using let

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,21 +1,32 @@
 require 'spec_helper'
 
 describe Location do
-	before { @location = FactoryGirl.create :location }
-	subject { @location }
+	let(:location) { FactoryGirl.create :location }
+	subject { location }
   describe 'method calls' do
   	it { should respond_to :name }
   	it { should respond_to :current_conditions }
-  	it { should respond_to :conditions_history }
+  	it { should respond_to :conditions }
+  end
+
+  describe 'validations' do
+  	specify "doesn't accept no name" do 
+  		invalid_location = Location.new()
+  		invalid_location.should_not be_valid 
+  	end
+  	specify "doesn't accept blank names" do
+  		invalid_location = Location.new(:name => "    ")
+  		invalid_location.should_not be_valid 
+  	end
   end
 
   describe 'current conditions' do
   	before { @conditions = FactoryGirl.create :conditions }
-  	before { @location.current_conditions = @conditions }
+  	before { location.current_conditions = @conditions }
 
   	it 'appends the current conditions to history' do
-  		@location.save 
-  		@location.conditions_history.last.should == @location.current_conditions
+  		location.save 
+  		location.conditions.last.should == location.current_conditions
   	end
   end
 end
