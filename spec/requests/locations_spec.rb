b37cf61c6c6f458e0516626f87c29209d5e36d8e
Brent <brenthaas+git@gmail.com>
2012-11-16 15:31:11 -0800

    specs for location detail

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -7,4 +7,23 @@ describe "Locations" do
       response.status.should be(200)
     end
   end
+
+  describe "overview" do
+  	let(:location) { FactoryGirl.create(:location, :with_conditions) }
+  	let(:conditions) { FactoryGirl.create(:conditions) }
+  	before { visit location_path(location.id) }
+  	subject {page}
+  	it "shows the current Temperature" do
+  		body.should have_content(conditions.temp)
+  	end
+  	it "shows the current Wind Speed" do
+  		body.should have_content(conditions.wind_speed)
+  	end
+  	it "shows the current Wind Direction" do
+  		body.should have_content(conditions.wind_direction)
+  	end
+  	it "has a link to history" do
+  		body.should have_link("History", :href => location_history_path(location))
+  	end
+  end
 end
