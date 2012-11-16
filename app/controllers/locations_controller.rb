56d6772db12799d319cc48de11fc1d8350268a32
Brent <brenthaas+git@gmail.com>
2012-11-16 14:35:39 -0800

    added history action and json response (removed json scaffolding entries)

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,35 +1,43 @@
 class LocationsController < ApplicationController
   # GET /locations
-  # GET /locations.json
   def index
     @locations = Location.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @locations }
     end
   end
 
   # GET /locations/1
-  # GET /locations/1.json
   def show
     @location = Location.find(params[:id])
     @conditions = @location.current_conditions
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @location }
+    end
+  end
+
+  # GET /locations/1/history
+  def history
+    @location = Location.find(params[:id])
+    if(params[:since])
+      @conditions_list = @location.conditions.where("created_at > ?", Time.at(params[:since].to_i))
+    else
+      @conditions_list = @location.conditions
+    end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @conditions_list, methods: [:formatted_time, :timestamp] }
     end
   end
 
   # GET /locations/new
-  # GET /locations/new.json
   def new
     @location = Location.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @location }
     end
   end
 
@@ -39,46 +47,38 @@ class LocationsController < ApplicationController
   end
 
   # POST /locations
-  # POST /locations.json
   def create
     @location = Location.new(params[:location])
 
     respond_to do |format|
       if @location.save
         format.html { redirect_to locations_url, notice: 'Location was successfully created.' }
-        format.json { render json: @location, status: :created, location: @location }
       else
         format.html { render action: "new" }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /locations/1
-  # PUT /locations/1.json
   def update
     @location = Location.find(params[:id])
 
     respond_to do |format|
       if @location.update_attributes(params[:location])
         format.html { redirect_to @location, notice: 'Location was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /locations/1
-  # DELETE /locations/1.json
   def destroy
     @location = Location.find(params[:id])
     @location.destroy
 
     respond_to do |format|
       format.html { redirect_to locations_url }
-      format.json { head :no_content }
     end
   end
 end
