3bad129dd101f5f62437f4bb259e9379836ec892
Brent <brenthaas+git@gmail.com>
2012-11-18 20:09:49 -0800

    added location name to json output for #index

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -3,7 +3,9 @@ class ConditionsController < ApplicationController
 		last_entry = params[:since] || 0
 		@conditions = Conditions.where("created_at > ?", Time.at(last_entry.to_i))
 		respond_to do |format| 
-      format.json { render json: @conditions, methods: [:formatted_time, :timestamp] }
+      format.json { render json: @conditions, 
+      											:include => {:location => {:only => :name}}, 
+      											:methods => [:formatted_time, :timestamp] }
     end
 	end	
 end
