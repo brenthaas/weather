efe7341fe0ff89d2cc3aac027b19c04c0fbc26af
Brent <brenthaas+git@gmail.com>
2012-11-24 13:49:43 -0800

    spec for location details

---
+++ spec/requests/location_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe '/location/:id' do
+  let(:location) { FactoryGirl.create(:location, :with_conditions) }
+  let(:conditions) { FactoryGirl.create(:conditions) }
+  before { visit location_path(location.id) }
+  subject {page.body}
+
+  describe "displayed info" do
+    it "shows the location name" do
+      should have_content(location.name)
+    end
+  	it "shows the current Temperature" do
+  		should have_content(conditions.temp)
+  	end
+  	it "shows the current Wind Speed" do
+  		should have_content(conditions.wind_speed)
+  	end
+  	it "shows the current Wind Direction" do
+  		should have_content(conditions.wind_direction)
+  	end
+  	it "has a link to history" do
+  		should have_link("History", :href => location_history_path(location))
+  	end
+  end
+end
